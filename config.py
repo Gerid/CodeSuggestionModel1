e1607ffbf66554231e3640cfb3fca526e2fc77b2
Gerid <915826939@qq.com>
2019-05-21 14:41:52 +0800

    add model part

--- config.py
+++ config.py
@@ -16,5 +16,4 @@ class ModelConfig(object):
 
 
 
-
 config = ModelConfig()
---
+++ model.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from config import config
+
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                  batch_input_shape=[batch_size, 50]
+                                  )
+    ]
+
+
+    )
